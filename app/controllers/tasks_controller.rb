935777d9443bfdb050f472e8781102f4a208fede
Kathy Kolonics <kata.kolonics@gmail.com>
2024-08-13 16:06:22 +0200

    implemented methods for updating and deleting

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,22 @@ class TasksController < ApplicationController
     @task.save
     redirect_to task_path(@task.id)
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task.id)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
 end
 
 private
